34f1eac0de24b68ce863130fa47377e35f841d6f
Tyler Russell <33195081+tyzoo@users.noreply.github.com>
2022-09-20 22:58:54 -0400

    captchas

---
+++ src/v1/controllers/captcha.ts
@@ -0,0 +1,62 @@
+import express from "express";
+import controllerLoader from "../middleware/controllerLoader";
+import svgCaptcha from "svg-captcha";
+import ironSession from "../middleware/ironSession";
+import { body } from "express-validator";
+
+/**
+ * Validation rules for this controller
+ */
+ const rules = {
+    body: {
+        code: {
+            required: 
+                body('code', 'code is required')
+                    .exists()
+                    .isLength({ min: 4 }),
+        },
+    },
+}
+/**
+ * Controller object returns array of middleware by key
+ */
+export default controllerLoader({
+
+    new: [
+        ironSession,
+        (async (req, res, next) => {
+            try {
+                const captcha = svgCaptcha.create();
+                req.session.captcha = captcha.text;
+                await req.session.save();
+                res.type('svg');
+                res.status(200).send(captcha.data);
+            } catch (err: any) {
+                res.status(500).json({
+                    message: `Error: ${err.message ?? "unknown error"}`,
+                })
+            }
+        }) as express.RequestHandler,
+    ],
+
+    verify: [
+        ironSession,
+        rules.body.code.required,
+        (async (req, res, next) => {
+            try {
+                const { code } = req.body;
+                const sessionCode = req.session.captcha
+                if( code && sessionCode && code === sessionCode ) {
+                    res.status(200).json({ success: true });
+                } else {
+                    res.status(403).json({ message: `Invalid code` });
+                }
+            } catch (err: any) {
+                res.status(500).json({
+                    message: `Error: ${err.message ?? "unknown error"}`,
+                })
+            }
+        }) as express.RequestHandler,
+    ],
+    
+});
